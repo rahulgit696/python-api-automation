53e116d6d33e17a3c0536d9eb80ef51289816876
rahulgit696 <rahulbadoni85@gmail.com>
2023-08-10 22:48:20 +0530

    Updated flow

---
+++ API/postMethods.py
@@ -0,0 +1,46 @@
+import requests
+from utilities.configurations import *
+
+# ADD CONFIGURATION AND URLS
+
+# config = get_config()
+# post_url = config['API']['endpoint'] + ApiResources.addBook
+# delete_urql = config['API']['endpoint'] + ApiResources.deleteBook
+# headers = {'Content-Type': 'application/json'}
+#
+# # ADD BOOK
+#
+# addbook_response = requests.post(post_url, json=add_book_payLoad("qwertytui"), headers=headers, )
+#
+# print(addbook_response.json())
+#
+# response_json = addbook_response.json()
+#
+# bookId = response_json['ID']
+# print(bookId)
+#
+# # DELETE BOOK
+#
+# response_delete_book = requests.post(delete_url, json={
+#     "ID": bookId
+# }, headers=headers, )
+#
+# assert response_delete_book.status_code == 200
+#
+# res_json = response_delete_book.json()
+#
+# print(res_json['msg'])
+#
+# assert res_json['msg'] == "book is successfully deleted"
+
+# SAMPLE AUTHENTICATION CHECK AND CREATING SESSION MANAGER
+
+session = requests.session()
+session.auth = auth = ('rahulgit696', get_password())
+url = 'https://api.github.com/user'
+git_response = requests.get(url, auth=('rahulgit696', get_password()))
+print(git_response.status_code)
+
+repo_url = 'https://api.github.com/user/repos'
+response = session.get(repo_url)
+print(response.status_code)
--- utilities/configurations.py
+++ utilities/configurations.py
@@ -5,3 +5,8 @@ def get_config():
     config = configparser.ConfigParser()
     config.read('utilities/properties.ini')
     return config
+
+
+def get_password():
+    password = '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'
+    return password
